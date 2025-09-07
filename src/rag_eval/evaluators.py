59c897e625e519a6c0d741c54b5aaed396d8f84f
benrodion <ben@anderer.de>
2025-09-07 12:45:17 +0200

    major bug fix for RetrievalPrecisionEvaluator

--- src/rag_eval/evaluators.py
+++ src/rag_eval/evaluators.py
@@ -98,7 +98,8 @@ class RetrievalPrecisionEvaluator:
         for i, doc_text in enumerate(ex.retrieved_texts):
             doc_supported_any = False
             for idx , c in enumerate(gt_claims):
-                print(f"GT Claim: {c}, Context: {doc_text}")
+                print(f" === ITERATION: {idx} ===")
+                #print(f"GT Claim: {c}, Context: {doc_text}") # just for bugfixing
                 verdict, _ = self.judge.judge(c, doc_text)
                 if verdict == "supported":
                     doc_supported_any = True
